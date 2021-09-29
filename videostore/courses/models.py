323c4ece29e00ab54f3d1f840b537087c22a661d
Сингулярность <killmagnuskill@gmail.com>
2021-09-29 23:34:33 +0300

    создана ссылка на магазин с товаром

--- videostore/courses/models.py
+++ videostore/courses/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Course(models.Model):
-    slug = models.SlugField() # для создания своего адреса URL
+    slug = models.SlugField()  # для создания своего адреса URL
     title = models.CharField(max_length=120)
     description = models.TextField()
     img = models.ImageField(default='default.png', upload_to='course_images')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('gems', kwargs={'slug': self.slug})
--- videostore/courses/urls.py
+++ videostore/courses/urls.py
@@ -1,9 +1,9 @@
 
-
-
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.HomePage.as_view(), name='home')
+    path('', views.HomePage.as_view(), name='home'),
+    path('shop/<slug>', views.ShopDetailPage.as_view(), name='gems')
 ]
