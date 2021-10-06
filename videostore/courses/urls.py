b1142d33e1c3664b4af4ae21b5e43fd3b9526066
Сингулярность <killmagnuskill@gmail.com>
2021-10-06 23:06:32 +0300

    добавил разграничение на платного и бесплатного контента

--- videostore/courses/urls.py
+++ videostore/courses/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
+    path('tarrifs/', views.HomePage.as_view(), name='tarrifs'),
     path('shop/<slug>', views.ShopDetailPage.as_view(), name='gems'),
     path('shop/<slug>/<shop_slug>', views.ShopSlugDetailPage.as_view(), name='shop-detail')
 ]
