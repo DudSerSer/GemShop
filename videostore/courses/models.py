dd1c4360c0758b7a1dac62fb93f33c2fb137235d
Сингулярность <killmagnuskill@gmail.com>
2021-10-06 22:31:50 +0300

    добавил просмотр раздела только авторизированным пользователям

--- videostore/courses/models.py
+++ videostore/courses/models.py
@@ -7,6 +7,8 @@ class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     img = models.ImageField(default='default.png', upload_to='course_images')
+    free = models.BooleanField(default=True)
+
 
     def get_absolute_url(self):
         return reverse('gems', kwargs={'slug': self.slug})
